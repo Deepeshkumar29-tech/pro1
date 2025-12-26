66f69e2bc386a28bd1ddfeed07da07d8ac8b1841
Deepesh Kumar <deepeshkumar1129@gmail.com>
2025-12-26 18:08:08 +0530

    Added a line

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+app.use(express.static(__dirname));
+
 
 // ------------------- DB CONNECTION -------------------
 
